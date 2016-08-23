703e0d2b8227d60e1cffc61f29dbabc38ded0144
Chris <cosma.chris@gmail.com>
2016-08-23 17:51:29 -0400

    changed javascript

--- push/js/main.js
+++ push/js/main.js
@@ -1,9 +1,13 @@
 if ('serviceWorker' in navigator) {
- console.log('Service Worker is supported');
- navigator.serviceWorker.register('sw.js').then(function(reg) {
-   console.log(':^)', reg);
-   // TODO
- }).catch(function(err) {
-   console.log(':^(', err);
- });
+    console.log('Service Worker is supported');
+    navigator.serviceWorker.register('sw.js').then(function(reg) {
+        console.log(':^)', reg);
+        reg.pushManager.subscribe({
+            userVisibleOnly: true
+        }).then(function(sub) {
+            console.log('endpoint:', sub.endpoint);
+        });
+    }).catch(function(error) {
+        console.log(':^(', error);
+    });
 }
