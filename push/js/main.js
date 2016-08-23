7a91d37051da1db054053486b58e4af616e1f0e0
Chris <cosma.chris@gmail.com>
2016-08-23 17:49:41 -0400

    started push notifications

--- push/js/main.js
+++ push/js/main.js
@@ -1,3 +1,9 @@
-'use strict';
-
-// TODO
+if ('serviceWorker' in navigator) {
+ console.log('Service Worker is supported');
+ navigator.serviceWorker.register('sw.js').then(function(reg) {
+   console.log(':^)', reg);
+   // TODO
+ }).catch(function(err) {
+   console.log(':^(', err);
+ });
+}
