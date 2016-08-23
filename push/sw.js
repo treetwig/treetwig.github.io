e4ebd24f18d6bf93b2c20581533f5d3326868710
Chris <cosma.chris@gmail.com>
2016-08-23 17:59:25 -0400

    changed sw.js

--- push/sw.js
+++ push/sw.js
@@ -16,3 +16,25 @@ self.addEventListener('push', function(event) {
       tag: 'my-tag'
     }));
 });
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('Notification click: tag ', event.notification.tag);
+    event.notification.close();
+    var url = 'https://youtu.be/gYMkEMCHtJ4';
+    event.waitUntil(
+        clients.matchAll({
+            type: 'window'
+        })
+        .then(function(windowClients) {
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
+});
