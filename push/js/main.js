a40af9f69db429abcabe43e23023efaeb3933d3c
Chris <cosma.chris@gmail.com>
2016-08-23 18:06:31 -0400

    updated code

--- push/js/main.js
+++ push/js/main.js
@@ -1,13 +1,42 @@
+var reg;
+var sub;
+var isSubscribed = false;
+var subscribeButton = document.querySelector('button');
 if ('serviceWorker' in navigator) {
-    console.log('Service Worker is supported');
-    navigator.serviceWorker.register('sw.js').then(function(reg) {
-        console.log(':^)', reg);
-        reg.pushManager.subscribe({
-            userVisibleOnly: true
-        }).then(function(sub) {
-            console.log('endpoint:', sub.endpoint);
-        });
-    }).catch(function(error) {
-        console.log(':^(', error);
-    });
+  console.log('Service Worker is supported');
+  navigator.serviceWorker.register('sw.js').then(function() {
+    return navigator.serviceWorker.ready;
+  }).then(function(serviceWorkerRegistration) {
+    reg = serviceWorkerRegistration;
+    subscribeButton.disabled = false;
+    console.log('Service Worker is ready :^)', reg);
+  }).catch(function(error) {
+    console.log('Service Worker Error :^(', error);
+  });
+}
+subscribeButton.addEventListener('click', function() {
+  if (isSubscribed) {
+    unsubscribe();
+  } else {
+    subscribe();
+  }
+});
+function subscribe() {
+  reg.pushManager.subscribe({userVisibleOnly: true}).
+  then(function(pushSubscription){
+    sub = pushSubscription;
+    console.log('Subscribed! Endpoint:', sub.endpoint);
+    subscribeButton.textContent = 'Unsubscribe';
+    isSubscribed = true;
+  });
+}
+function unsubscribe() {
+  sub.unsubscribe().then(function(event) {
+    subscribeButton.textContent = 'Subscribe';
+    console.log('Unsubscribed!', event);
+    isSubscribed = false;
+  }).catch(function(error) {
+    console.log('Error unsubscribing', error);
+    subscribeButton.textContent = 'Subscribe';
+  });
 }
