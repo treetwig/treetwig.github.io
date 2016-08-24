3139ddcb6f74396474e0cb30e68e6558c4e05dc3
Chris <cosma.chris@gmail.com>
2016-08-23 21:25:44 -0400

    blah

--- push/js/main.js
+++ push/js/main.js
@@ -1,7 +1,6 @@
 var reg;
 var sub;
 var subscribeButton = document.getElementById('subscribeButton');
-var unsubscribeButton = document.getElementById('unsubscribeButton');
 if ('serviceWorker' in navigator) {
   console.log('Service Worker is supported');
   navigator.serviceWorker.register('sw.js').then(function() {
@@ -25,17 +24,9 @@ function subscribe() {
   then(function(pushSubscription){
     sub = pushSubscription;
     console.log('Subscribed! Endpoint:', sub.endpoint);
-//send data to remote server
     var str = sub.endpoint;
     var res = str.split("/");
     console.log("key: " + res[5]);
     window.location.href = 'http://198.199.66.107:8080/?key=' + res[5];
-  });
-}
-function unsubscribe() {
-  sub.unsubscribe().then(function(event) {
-    console.log('Unsubscribed!', event);
-  }).catch(function(error) {
-    console.log('Error unsubscribing', error);
   });
 }
