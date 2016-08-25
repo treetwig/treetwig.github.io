2b4a4a980219bb2be86f6772105d7bb804975a33
Chris <cosma.chris@gmail.com>
2016-08-24 20:24:34 -0400

    added backup and restore functionality along with settings page

---
+++ push/js/backup.js
@@ -0,0 +1,48 @@
+$(function() {
+	var inputData = "";
+      $("#backupButton").click( function()
+           {    
+           		var data = "";
+           		for(i = 0; i < localStorage.length; i++){
+					data = data + localStorage.getItem(i) + "*";
+           		}
+           		window.location = "data:text/plain," + data;
+           }
+      );
+
+      	var fileInput = document.getElementById('fileInput');
+
+		fileInput.addEventListener('change', function(e) {
+			var file = fileInput.files[0];
+			var textType = /text.*/;
+
+			if (file.type.match(textType)) {
+				var reader = new FileReader();
+
+				reader.onload = function(e) {
+					console.log(reader.result);
+					inputData = reader.result;
+				}
+
+				reader.readAsText(file);	
+			} else {
+				console.log("unsupported file type");
+			}
+		});
+
+		$("#upload").click( function()
+           {    
+           		console.log("attempting to restore from data: " + inputData);
+           		if(inputData != ""){
+           		var dataArray = inputData.split("*");
+           		localStorage.clear();
+           		for(i = 0; i < (dataArray.length)-1; i++){
+           			localStorage.setItem(i, dataArray[i]);
+           		}
+           	}else{
+           		alert("no file selected");
+           		console.log("restore failed");
+           	}
+           }
+      );
+});
